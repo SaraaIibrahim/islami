c486bcfa13d3b704cfa67b45e556ad4bd4669203
SARA <saso0507@gmail.com>
2022-08-18 17:12:43 +0200

    initialize project add empty home screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
   routes: {
-    HomeScreen.routeName :(_)=>HomeScreen()
+     HomeScreen.routeName :(_)=>HomeScreen()
   },
       initialRoute:HomeScreen.routeName ,
     );
